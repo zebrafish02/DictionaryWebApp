0b01abd2d21e51c9e2c0659b91f315bcaaaed267
zebrafish02 <91638292+zebrafish02@users.noreply.github.com>
2025-01-17 09:25:19 -0600

    👍
    !!!

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -29,35 +29,61 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
+            this.home_button = new System.Windows.Forms.Button();
+            this.history_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // home_button
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(123, 118);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = false;
+            this.home_button.Font = new System.Drawing.Font("Nirmala UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.home_button.Image = ((System.Drawing.Image)(resources.GetObject("home_button.Image")));
+            this.home_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.home_button.Location = new System.Drawing.Point(12, 7);
+            this.home_button.Name = "home_button";
+            this.home_button.Size = new System.Drawing.Size(114, 36);
+            this.home_button.TabIndex = 0;
+            this.home_button.Text = "Home";
+            this.home_button.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.home_button.UseVisualStyleBackColor = true;
+            // 
+            // history_button
+            // 
+            this.history_button.Image = ((System.Drawing.Image)(resources.GetObject("history_button.Image")));
+            this.history_button.Location = new System.Drawing.Point(749, 12);
+            this.history_button.Name = "history_button";
+            this.history_button.Size = new System.Drawing.Size(39, 35);
+            this.history_button.TabIndex = 1;
+            this.history_button.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(314, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(794, 530);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.history_button);
+            this.Controls.Add(this.home_button);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button home_button;
+        private System.Windows.Forms.Button history_button;
+        private System.Windows.Forms.Label label1;
     }
 }
 
